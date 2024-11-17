b6427008b392edb6fa06586e727cfc1c23be892c
Kuba <jakub.paczko01@gmail.com>
2024-11-17 16:34:15 +0100

    imroved like and comment count

--- Base/models.py
+++ Base/models.py
@@ -24,7 +24,7 @@ class Community(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=256, blank=False, default="Title")
     content = models.CharField(max_length=1024, blank=False)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_DEFAULT, default=None, related_name="post_author")
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_DEFAULT, default=None, related_name="posts")
     pub_date = models.DateTimeField(default=datetime.now(), blank=True)
     image = models.ImageField(blank=True, null=True, upload_to='images/')
     community = models.ForeignKey(Community, on_delete=models.CASCADE, default=None)
@@ -34,19 +34,16 @@ class Post(models.Model):
     
     @property
     def get_like_count(self):
-        likes = PostLike.objects.filter(post=self)
-        return likes.count()
-
+        return self.likes.all().count()
+    
     @property
     def get_comment_count(self):
-        comments = Comment.objects.filter(post=self)
-        return comments.count()
-        # return PostLike.objects.filter(post=post)
+        return self.comments.all().count()
 
 class Comment(models.Model):
     content = models.CharField(max_length=1024)
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None, related_name='comments')
     date = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
@@ -54,8 +51,8 @@ class Comment(models.Model):
  #   likes = models.ManyToMany#Field(User, through="Like")
 
 class PostLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_by")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_post")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
     date_time = models.DateTimeField(default=datetime.now())
 
     class Meta:
