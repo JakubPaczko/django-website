227da397a078105b6db24e8f014961ef366454d2
Jakub <108963654+JakubPaczko@users.noreply.github.com>
2024-12-02 15:07:53 +0100

    Delete Base directory

--- Base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Comment, Post, Community, PostLike
-
-admin.site.register(User)
-admin.site.register(Comment)
-admin.site.register(Post)
-admin.site.register(Community)
-admin.site.register(PostLike)
