0829881187bc898c632a9b160be247fbf21ceb46
Kuba <jakub.paczko01@gmail.com>
2024-12-02 15:09:01 +0100

    reorganization

--- Base/serlializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from Base.models import Post
-
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
